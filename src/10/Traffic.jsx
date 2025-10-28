c98738c21aba0dca99bed691da1a0cf262d996ff
pnumin <pnumin@pusan.ac.kr>
2025-10-28 12:32:25 +0900

    교통사고조회

--- src/10/Traffic.jsx
+++ src/10/Traffic.jsx
@@ -1,6 +1,6 @@
 import TrafficNav from "./TrafficNav"
 import TrafficInfo from "./TrafficInfo";
-import trafficData from "./교통사고통계.json"
+// import trafficData from "./교통사고통계.json"
 
 import { useState, useEffect } from "react"
 
@@ -19,8 +19,15 @@ export default function Traffic() {
   //사고데이터
   const [tInfo, setTinfo] = useState([]) ;
 
-  const getFetchData = () => {
-    setTdata(trafficData) ;
+  const getFetchData = async () => {
+    const apikey = import.meta.env.VITE_API_KEY ;
+    const baseUrl = "https://api.odcloud.kr/api/15070282/v1/uddi:8449c5d7-8be5-4712-9093-968fc0b2d9fc?" ;
+    let url = `${baseUrl}page=1&perPage=117&returnType=json&serviceKey=${apikey}` ;
+
+    const resp = await fetch(url) ;
+    const data = await resp.json() ;
+    setTdata(data.data) ;
+  
   }
 
   useEffect(() => {
