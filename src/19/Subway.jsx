4c0a93eb0f26029ca1dd046377a77cbc85379905
pnumin <pnumin@pusan.ac.kr>
2025-11-13 13:12:23 +0900

    지하철 대기 정보조회

--- src/19/Subway.jsx
+++ src/19/Subway.jsx
@@ -1,16 +1,39 @@
 import sarea from "./sarea.json"
-import scode from "./scode.json"
 import SubwayBox from "./SubwayBox"
 import TailSelect from "../components/TailSelect"
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef, use } from "react"
 
 export default function Subway() {
   const [tdata, setTdata] = useState([]) ;
   const selAreaRef = useRef() ;
 
+  const getFetchData  = async () => {
+    const dt = new Date().toISOString().slice(0, 10).replaceAll('-', '') ;
+    // //https://apis.data.go.kr/6260000/IndoorAirQuality/getIndoorAirQualityByStation?serviceKey=8qw7g%2FC%2BMGd2iRqEvb%2FEx0Sg3ZwAAsnS%2FQ7rRaU3l4UUYfNWgyAbYpNw541yy9pueEvoCcNwmCww8ss32BBWEA%3D%3D&pageNo=1&numOfRows=5&resultType=json&controlnumber=2025111306&areaIndex=201193
+    const apikey = import.meta.env.VITE_API_KEY ;
+    const baseUrl = "/api/6260000/IndoorAirQuality/getIndoorAirQualityByStation?" ;
+    let url = `${baseUrl}serviceKey=${apikey}&pageNo=1&numOfRows=50`;
+    url =  `${url}&resultType=json&controlnumber=${dt}&areaIndex=${selAreaRef.current.value}`;
+
+    console.log(url)
+    const resp = await fetch(url) ;
+    const data = await resp.json() ;
+
+    let tm = data.response.body.items.item ;
+    tm = tm.sort((a, b) => a.controlnumber - b.controlnumber) ;
+    setTdata(tm);
+  }
+
   const handelSelect = () => {
     console.log(selAreaRef.current.value)
+    if(!selAreaRef.current.value ) return ;
+    getFetchData() ;
+
   }
+
+  useEffect(() => {
+    console.log(tdata)
+  } , [tdata]);
   return (
     <div className="w-9/10 flex flex-col justify-start mt-10">
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -24,7 +47,10 @@ export default function Subway() {
                     opv={sarea.map(item => item["측정소"])} 
                     onHandle={handelSelect} />
       </div>
-      <SubwayBox />
+      { tdata && tdata.map((item, idx) => <SubwayBox key={item.controlnumber}
+                                              idx={idx % 2}
+                                              item={item} />)}
+      
     </div>
   )
 }
--- src/19/SubwayBox.jsx
+++ src/19/SubwayBox.jsx
@@ -1,9 +1,36 @@
-
-
-export default function SubwayBox() {
+import scode from "./scode.json";
+export default function SubwayBox({ idx , item }) {
+  console.log(item)
+  console.log(Object.keys(scode))
   return (
-    <div>
-      SubwayBox
+    <div className="w-full flex flex-col justify-start my-2">
+      <div className={`w-full px-5
+                        ${idx == 0 ? "text-lime-800" : "text-amber-800"} 
+                    font-bold`}>
+        {item.office} {item.site} {item.city}
+        (
+        {item.controlnumber.slice(0, 4)}.
+        {item.controlnumber.slice(4, 6)}.
+        {item.controlnumber.slice(6, 8)} &nbsp;
+        {item.controlnumber.slice(8, 10)}시)
+      </div>
+      <div className="w-full grid grid-cols-2 md:grid-cols-5 lg:grid-cols-9 gap-2">
+        {
+          Object.keys(scode).map(c => <div key={c} className="w-full flex flex-col"> 
+                                        <div className={`${idx == 0 ? "bg-lime-800" : "bg-amber-800"}
+                                                        text-white p-2
+                                                        font-bold text-sm
+                                                        flex flex-col justify-center items-center`}>
+                                          <div>{scode[c]["name"]}</div>
+                                          <div>({c})</div>
+                                        </div>
+                                        <div className={` border p-2 text-center
+                                                          ${idx == 0 ? "border-lime-800" : "border-amber-800"}`}>
+                                         {item[c]} {item[c] == '-' ? '' : scode[c]["unit"]}
+                                        </div>
+                                      </div>)
+        }
+      </div>
     </div>
   )
 }
